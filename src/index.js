5f7c5b87503bcb75e14565ea83c2e794662abda4
noria_t <55936478+noria6t@users.noreply.github.com>
2021-10-03 16:01:55 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -48,3 +48,51 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+// /**
+//  * テンプレート文字列
+//  */
+// const name = "じゃけぇ";
+// const age = 28;
+
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str){
+//   return str;
+// }
+// console.log(func1("func1です"))；
+
+// 変数に関数を代入
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// アロー関数
+// (str)はstrでも良い
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です。"));
+
+// アロー関数
+// 中が一行ならreturn省略
+// const func2 = (str) => str;
+// console.log(func2("func2です。"));
+
+// アロー関数
+// 足し算例
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
